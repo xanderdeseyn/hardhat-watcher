781aba7ec6b86b1031fbffea3315af08a59c1b63
Xander Deseyn <xander@devizer.io>
2020-10-30 13:03:48 +0100

    Better tests

--- test/project.test.ts
+++ test/project.test.ts
@@ -8,35 +8,33 @@ import {
   simulateFileChange,
 } from "./helpers";
 
-describe("Integration tests examples", function () {
-  describe("Happy case", function () {
-    this.timeout(120_000);
-    useEnvironment("hardhat-project");
+describe("Watcher tests", function () {
+  this.timeout(15000);
+  useEnvironment("hardhat-project");
 
-    beforeEach(async function () {
-      await this.hre.run("clean");
-    });
+  beforeEach(async function () {
+    await this.hre.run("clean");
+  });
 
-    it("Can run watcher", async function () {
-      try {
-        simulateFileChange("Contract.sol");
-        await this.hre.run("watch");
-        await sleep(1000);
+  it("Watch contracts and clean + compile on change", async function () {
+    try {
+      simulateFileChange("Contract.sol");
+      await this.hre.run("watch");
+      await sleep(1000);
 
-        console.log("=========== Simulating file change =============");
-        simulateFileChange("Contract2.sol");
-        await sleep(2000);
-        const dir = readArtifactDir(this.hre);
-        expect(dir).to.contain("TestContract2.json");
+      console.log("=========== Simulating file change =============");
+      simulateFileChange("Contract2.sol");
+      await sleep(2000);
+      const dir = readArtifactDir(this.hre);
+      expect(dir).to.contain("TestContract2.json");
 
-        console.log("=========== Simulating file change =============");
-        simulateFileChange("Contract3.sol");
-        await sleep(2000);
-        const dir2 = readArtifactDir(this.hre);
-        expect(dir2).to.contain("TestContract3.json");
-      } catch (error) {
-        assert.fail(error.message);
-      }
-    });
+      console.log("=========== Simulating file change =============");
+      simulateFileChange("Contract3.sol");
+      await sleep(2000);
+      const dir2 = readArtifactDir(this.hre);
+      expect(dir2).to.contain("TestContract3.json");
+    } catch (error) {
+      assert.fail(error.message);
+    }
   });
 });
